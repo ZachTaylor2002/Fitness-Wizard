8b0b1521aa7ec20ad71f39511e607b15f591c869
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-06-24 13:32:16 -0500

    Created three React compoenets of Hero, Generator, and Workout to be used in the App

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+//First React Component created called Hero
+//This component has the same name as the file (as well as the function) 
+
+export default function Hero() {
+  return (
+    <div>Hero</div>
+  )
+}
