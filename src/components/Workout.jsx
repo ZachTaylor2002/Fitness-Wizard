6b303f3e75fad312e311de101c1f33a96ee4aa07
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-06-30 11:20:56 -0500

    Created a div section inside of Workout component where the child component Excercise card will be rendered

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -5,6 +5,10 @@ export default function Workout(props) {
   const { workout } = props
   return (
     <SectionWrapper header={"Ready to Crush it?"} title={['Your', 'Workout', 'Awaits']}>
+        <div className='flex flex-col gap-4'>
+        
+        
+        </div>
         </SectionWrapper>
   )
 }
