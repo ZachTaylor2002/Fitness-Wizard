bc103d2b54271ad8224d11d9b72deb4f283d339d
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-06-30 11:28:57 -0500

    Fixed a typo with the name of ExerciseCard and inside of the Workout Component passed in the excerise and the corresponding key index

--- src/components/ExcerciseCard.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ExcerciseCard() {
-  return (
-    <div>ExcerciseCard</div>
-  )
-}
--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import SectionWrapper from './SectionWrapper'
+import SectionWrapper from './SectionWrapper.jsx'
+import ExerciseCard from './ExerciseCard.jsx'
 
 export default function Workout(props) {
   const { workout } = props
   return (
     <SectionWrapper header={"Ready to Crush it?"} title={['Your', 'Workout', 'Awaits']}>
         <div className='flex flex-col gap-4'>
-        
-        
+        {workout.map((exercise, i) => {
+                    return (
+                        <ExerciseCard i={i} exercise={exercise} key={i} />
+                    )
+                })}
+
         </div>
         </SectionWrapper>
   )
