d461b93ac74a7c9dd98d28ce87f5aebd531e0d32
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-06-30 11:53:50 -0500

    In the Excercise component, passed in the index of I and added some background styling

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 
 export default function ExerciseCard(props) {
-  const {excercise} = props 
+  const {excercise, i} = props 
   return (
-    <div>ExerciseCard</div>
+    <div className='p-4 rounded-md flex flex flex-col gap-4 bg-slate-950 sm:flex-wrap'>
+        <div className='flex flex-col sm:flex-row sm:items-center sm:flex-wrap gap-x-4'>
+          <h4 className='text-3xl hidden sm:inline sm:text-4xl md:text-5xl font-semibold text-slate-400'>
+               0{i + 1}
+          </h4>
+         </div>
+    </div>
   )
 }
--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import SectionWrapper from './SectionWrapper.jsx'
-import ExerciseCard from './ExerciseCard.jsx'
+import React from 'react';
+import SectionWrapper from './SectionWrapper.jsx';
+import ExerciseCard from './ExerciseCard.jsx';
 
 export default function Workout(props) {
-  const { workout } = props
+  const { workout } = props;
   return (
-    <SectionWrapper header={"Ready to Crush it?"} title={['Your', 'Workout', 'Awaits']}>
-        <div className='flex flex-col gap-4'>
-        {workout.map((exercise, i) => {
-                    return (
-                        <ExerciseCard i={i} exercise={exercise} key={i} />
-                    )
-                })}
-
-        </div>
-        </SectionWrapper>
-  )
+    <SectionWrapper header={'Ready to Crush it?'} title={['Your', 'Workout', 'Awaits']}>
+      <div className='flex flex-col gap-4'>
+        {workout.map((exercise, i) => (
+          <ExerciseCard i={i} exercise={exercise} key={i} />
+        ))}
+      </div>
+    </SectionWrapper>
+  );
 }
